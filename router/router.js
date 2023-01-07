838fd83d6ebde3ada47c007d380ea407c4b2dff9
Vladlen Branetskiy <vbranetskiy@rightandabove.com>
2023-01-07 23:06:59 +0200

    main
    add short urls functional

--- router/router.js
+++ router/router.js
@@ -1,10 +1,6 @@
 const router = require('express').Router()
 const { urlRouter } = require('../src/urls')
 
-// TODO
-// 1) Auth
-// 2) Users
-// 3) users by user
 router.use('/url', urlRouter)
 
 module.exports = router
